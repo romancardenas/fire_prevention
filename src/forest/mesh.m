8277ade4732674e826dc88349745a5d18636b29f
Román Cárdenas <rcardenas.rod@gmail.com>
2018-11-14 13:45:55 +0100

    mesh grid algorithm fixed

--- src/forest/mesh.m
+++ src/forest/mesh.m
@@ -9,6 +9,7 @@ MAILBOX = 1;
 KNOWLEDGE = 2;
 TOSEND = 3;
 
+% If a sensor has something to say, it copies it to its own TOSEND table
 for i = 1:m
     for j = 1:n
         if world_sensors(i, j) ~= 0
@@ -18,45 +19,44 @@ for i = 1:m
         end
     end
 end
+% Initially, the KNOWLEDGE table is the same as the TOSEND table
 fucked_up_thing(:, :, KNOWLEDGE, :, :) = fucked_up_thing(:, :, TOSEND, :, :);
-%% TODO the original sensor forgets its value!!!
+
+% Communication protocol takes place as far as the protocol steps allow it
 for step = 1:max_jumps
-    % to send
+    % Sensors copy their TOSEND table to the MAILBOX table of their neighbors
     for i = 1:m
         for j = 1:n
-            if sum(sum(fucked_up_thing(i, j, TOSEND, i, j))) ~= 0
-                msg = fucked_up_thing(:, :, TOSEND, i, j);
-                %world_sensors(i, j).send()  % reduce battery
-                for k = -1:1
-                    row = i + k;
-                    if (row > 0) || (row <= m)
-                        for l = -1:1
-                            col = j + l;
-                            if (col > 0) || (col <= n) || (row ~= i && col ~= j)
-                                % TODO avoid auto erasing
-                                mailbox = fucked_up_thing(:, :, MAILBOX, row, col);
-                                fucked_up_thing(:, :, MAILBOX, row, col) = double(mailbox == 0) .* msg + mailbox;
-                            end
-                        end
+            if sum(sum(fucked_up_thing(:, :, TOSEND, i, j))) == 0
+                continue
+            end
+            msg = fucked_up_thing(:, :, TOSEND, i, j);
+            %world_sensors(i, j).send()  % reduce battery
+            for k = -1:1
+                row = i + k;
+                if (row < 1) || (row > m)
+                    continue
+                end
+                for l = -1:1
+                    col = j + l;
+                    if (col < 1) || (col > n)
+                        continue
                     end
+                    mailbox = fucked_up_thing(:, :, MAILBOX, row, col);
+                    fucked_up_thing(:, :, MAILBOX, row, col) = double(mailbox == 0) .* msg + mailbox;
                 end
             end
         end
     end
-    %read mailbox and prepare to send
-    for i = 1:m
-        for j = 1:n
-            mailbox = fucked_up_thing(:, :, MAILBOX, i, j);
-            knowledge = fucked_up_thing(:, :, KNOWLEDGE, i, j);
-            
-            tosend = double(knowledge == 0).*mailbox;
-            knowledge = tosend + knowledge .* double(mailbox==0);
-            
-            fucked_up_thing(:, :, TOSEND, i, j) = tosend;
-            fucked_up_thing(:, :, KNOWLEDGE, i, j) = knowledge;
-        end
-    end
+    
+    % Each sensor copies new info in MAILBOX table to KNOWLEDGE and TOSEND tables
+    mailbox = fucked_up_thing(:, :, MAILBOX, :, :);
+    knowledge = fucked_up_thing(:, :, KNOWLEDGE, :, :);
+    tosend = double(knowledge == 0) .* mailbox;
+    knowledge = tosend + knowledge;
+    fucked_up_thing(:, :, TOSEND, :, :) = tosend;
+    fucked_up_thing(:, :, KNOWLEDGE, :, :) = knowledge;
     fucked_up_thing(:, :, MAILBOX, :, :) = 0;
 end
+%% TODO Check the information in the edges and return all the messages that made it
 end
-
